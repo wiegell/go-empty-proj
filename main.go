effd96fa01c0dbf7b08af0251f07e3d697bce79b
BWiegell <bj.wiegell@gmail.com>
2023-02-27 00:08:31 +0100

    added nodemon command

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+type teststruct struct {
+	 content string
+}
+
 func helloworld() string {
-	return "Hello World!!"
+	testvar := teststruct{content: "Hello World"}
+	testvar2 := 2;
+	return testvar.content + fmt.Sprint(testvar2) + "!!"
 }
 
 func main() {
